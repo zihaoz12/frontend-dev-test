db17f52a41eec2a7916fdec9f5f4aaa82fab7b70
zihaoz12 <zzhaa12@gmail.com>
2019-07-14 01:34:06 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Home from '../index.html'
+import Styles from './scss/app.scss'
+import 'bootstrap'
+
+
+console.log("working")
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './src/app.js',
+
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	watch: true,
+	devServer: {
+		inline: true
+	},
+	//Add sass-loader
+	module: {
+		rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      },
+			{
+				test: /\.(scss)$/,
+				use: [{
+					loader: 'style-loader'
+				}, {
+					loader: 'css-loader'
+				}, {
+					loader: 'sass-loader'
+				}, {
+					loader: 'postcss-loader',
+					options: {
+						plugins: function () {
+							return [ require('autoprefixer')]
+						}
+					}
+				}]
+			},
+			{
+				test:/\.(png|jpg|gif)$/,
+				use:[{
+					loader:'url-loader',
+					options:{
+						limit:5000
+					}
+				}]
+			},
+			{
+				test:/\.svg$/,
+				use:['file-loader']
+			}
+		]
+	}
+}
