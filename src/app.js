a2ab365aa97dd00afc78fddd33c3fda933fc1b9d
zihaoz12 <zzhaa12@gmail.com>
2019-07-15 01:01:09 -0700

    basic style

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import Home from '../index.html'
 import Styles from './scss/app.scss'
 import 'bootstrap'
+require('expose-loader?$!jquery');
 
 
-console.log("working")
+console.log("working");
+
+$(function(){
+    $(document).scroll(function(){
+    let $nav = $("#headerNav");
+    $nav.toggleClass("scrolled",$(this).scrollTop() > $nav.height());
+  })
+});
+
+// $(function(){
+// 	$(window).scroll(function(){
+// 		let top1 = $(this).scrollTop();
+// 		if(top1>300){
+// 			$("#backToTop").stop().fadeIn().css("display","block");
+// 		}else{
+// 			$("#backToTop").stop().fadeOut();
+// 		}
+// 	});
+
+// 	$("#backToTop").click(function(){
+// 		$(" body, html").animate({
+// 			scrollTop:0
+// 		},300);
+// 	});	
+// });
